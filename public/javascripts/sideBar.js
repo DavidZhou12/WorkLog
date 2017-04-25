2cdc4db8ec8d47effaa982776252c5899aaff9c9
David Zhou <DavidZhouToday@gmail.com>
2017-04-25 02:35:25 -0400

    Add function to display date in calendar panel

--- public/javascripts/sideBar.js
+++ public/javascripts/sideBar.js
@@ -2,8 +2,8 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
-//Loads the correct sidebar on window load,
-//collapses the sidebar on window resize.
+// Loads the correct sidebar on window load,
+// collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 $(function() {
     $(window).bind("load resize", function() {

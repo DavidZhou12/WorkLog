cebc88ffcb98a19194d47f3f3226bb86a9e431e1
David Zhou <DavidZhouToday@gmail.com>
2017-05-08 21:10:32 -0400

    Add comment for static folder

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Set Static Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);

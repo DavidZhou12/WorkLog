2364a3bf387f4254962816b58086e11d36ffbb79
David Zhou <DavidZhouToday@gmail.com>
2017-05-09 19:55:19 -0400

    Check register forms

--- routes/register.js
+++ routes/register.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('register', { title: 'Register' });
 });
 
+router.post('/', function(req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password1 = req.body.password1;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+
+	var errors = req.validationErrors();
+
+	if(errors) {
+		console.log('YES');
+	} else {
+		console.log('NO');
+	}
+});
+
 module.exports = router;

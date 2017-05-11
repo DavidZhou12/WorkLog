4c9b786778fc91fb8efcbc6ada2a70b8af548e26
David Zhou <DavidZhouToday@gmail.com>
2017-05-11 03:37:16 -0400

    Add passport login authentication functions

--- models/user.js
+++ models/user.js
@@ -27,3 +27,20 @@ module.exports.createUser = function(newUser, callback){
 	    });
 	});
 }
+
+// The User's email will be the 'username'
+module.exports.getUserByUsername = function(username, callback){
+	var query = {email: username};
+	User.findOne(query, callback);
+}
+
+module.exports.getUserById = function(id, callback){
+	User.findById(id, callback);
+}
+
+module.exports.comparePassword = function(candidatePassword, hash, callback){
+	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
+    	if(err) throw err;
+    	callback(null, isMatch);
+	});
+}
--- routes/login.js
+++ routes/login.js
@@ -8,4 +8,30 @@ router.get('/', function(req, res, next) {
   res.render('login', { title: 'Login' });
 });
 
+passport.use(new LocalStrategy(
+	function(username, password, done) {
+	User.getUserByUsername(username, function(err, user){
+		if(err) throw err;
+		if(!user){
+			return done(null, false, {message: 'Unknown User'});
+		}
+
+		User.comparePassword(password, user.password, function(err, isMatch){
+			if(err) throw err;
+			if(isMatch){
+				return done(null, user);
+			} else {
+				return done(null, false, {message: 'Invalid password'});
+			}
+		});
+	});
+}));
+
+router.post('/',
+	passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}),
+	function(req, res) {
+		res.redirect('/');
+	}
+);
+
 module.exports = router;

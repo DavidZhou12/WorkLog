e7fb6c5b3fbecedd0d46ef15beddd8f2f0d63370
David Zhou <DavidZhouToday@gmail.com>
2017-05-11 03:35:53 -0400

    Add connect-flash success_msg

--- routes/register.js
+++ routes/register.js
@@ -44,7 +44,7 @@ router.post('/', function(req, res) {
 			console.log(user);
 		});
 
-		//req.flash('success_msg', 'You are registered and can now login');
+		req.flash('success_msg', 'You are registered and can now login');
 
 		res.redirect('/login');
 	}

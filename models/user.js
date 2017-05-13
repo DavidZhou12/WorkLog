1770a8e32be75283cf4b7858bc57c2511fa00f97
David Zhou <DavidZhouToday@gmail.com>
2017-05-13 02:00:29 -0400

    Fix spacing

--- models/user.js
+++ models/user.js
@@ -26,8 +26,7 @@ var UserSchema = mongoose.Schema({
 var User = module.exports = mongoose.model('User', UserSchema);
 
 // User functions
-module.exports.createUser = function(newUser, callback){
-	newUser.admin = false;
+module.exports.createUser = function(newUser, callback) {
 	bcrypt.genSalt(10, function(err, salt) {
 	    bcrypt.hash(newUser.password, salt, function(err, hash) {
 	        newUser.password = hash;
@@ -37,16 +36,16 @@ module.exports.createUser = function(newUser, callback){
 }
 
 // The User's email will be the 'username'
-module.exports.getUserByUsername = function(username, callback){
+module.exports.getUserByUsername = function(username, callback) {
 	var query = {email: username};
 	User.findOne(query, callback);
 }
 
-module.exports.getUserById = function(id, callback){
+module.exports.getUserById = function(id, callback) {
 	User.findById(id, callback);
 }
 
-module.exports.comparePassword = function(candidatePassword, hash, callback){
+module.exports.comparePassword = function(candidatePassword, hash, callback) {
 	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
     	if(err) throw err;
     	callback(null, isMatch);

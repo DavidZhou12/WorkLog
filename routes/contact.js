c7bef5558634c470e98c3cf925bd4809cf058df4
David Zhou <DavidZhouToday@gmail.com>
2017-05-12 23:37:26 -0400

    Add functions.js file for ensureAutheticated

--- routes/contact.js
+++ routes/contact.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const functions = require('./functions.js');
 
 /* GET contact page. */
-router.get('/', ensureAuthenticated, function(req, res) {
+router.get('/', functions.ensureAuthenticated, function(req, res) {
   res.render('contact', { title: 'Contact Us!' });
 });
-
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()) {
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/login');
-	}
-}
 
 module.exports = router;
--- routes/guideBackup.js
+++ routes/guideBackup.js
@@ -1,18 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const functions = require('./functions.js');
 
 /* GET guide page. */
-router.get('/', ensureAuthenticated, function(req, res) {
+router.get('/', functions.ensureAuthenticated, function(req, res) {
   res.render('guideBackup', { title: 'How to Backup Computers' });
 });
-
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()) {
-		return next();
-	} else {
-		//req.flash('error_msg','You are not logged in');
-		res.redirect('/login');
-	}
-}
 
 module.exports = router;

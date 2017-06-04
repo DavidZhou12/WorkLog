6936c45295bca0e478609055769d2e314609721a
David Zhou <DavidZhouToday@gmail.com>
2017-06-04 19:13:15 -0400

    Show proper date

--- routes/calendar.js
+++ routes/calendar.js
@@ -4,7 +4,16 @@ const functions = require('./functions.js');
 
 /* GET calendar page. */
 router.get('/', functions.ensureAuthenticated, function(req, res) {
-  res.render('calendar', { title: 'Calendar' });
+	const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const currentDate = new Date();
+        const currentMonth = monthName[currentDate.getMonth()];
+        const currentDay = currentDate.getDate();
+        const currentYear = currentDate.getFullYear();
+	res.render('calendar', { title: 'Calendar',
+							 month: currentMonth,
+							 day: currentDay,
+							 year: currentYear
+						   });
 });
 
 module.exports = router;
